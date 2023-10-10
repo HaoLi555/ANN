35a781608d31dc5f124a8adfc4bdee0f7b9c4758
absofkinlutelihao <hao-li21@mails.tsinghua.edu.cn>
2023-10-10 08:12:18 +0800

    feat: README for HW1

--- HW1/run.sh
+++ HW1/run.sh
@@ -1,3 +1,7 @@
-python3 run_mlp.py -n 3 -a Si -l M
-python3 run_mlp.py -n 3 -a Se -l M
-python3 run_mlp.py -n 3 -a Ge -l F
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a R -l M
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a Si -l M
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a Se -l M
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a Ge -l M
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a Sw -l M
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a R -l S
+python3 /home/haoooooooooooookkkkk/sophomore_autumn/ANN/HW1/codes/run_mlp.py -n 2 -a Si -l S
