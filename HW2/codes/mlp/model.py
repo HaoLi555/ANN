90e054c24bcdeb41bd1844b4bcd00bfafc5c08aa
absofkinlutelihao <hao-li21@mails.tsinghua.edu.cn>
2023-10-23 11:40:21 +0800

    fix: sequential in bn, mlp

--- HW2/codes/mlp/model.py
+++ HW2/codes/mlp/model.py
@@ -4,78 +4,86 @@ import torch
 from torch import nn
 from torch.nn import init
 from torch.nn.parameter import Parameter
+from typing import OrderedDict
+
+
 class BatchNorm1d(nn.Module):
-	# TODO START
-	def __init__(self, num_features, momentum=0.9):
-		super(BatchNorm1d, self).__init__()
-		self.num_features = num_features
-		self.momentum=momentum
+    # TODO START
+    def __init__(self, num_features, momentum=0.9):
+        super(BatchNorm1d, self).__init__()
+        self.num_features = num_features
+        self.momentum = momentum
 
-		# Parameters
-		self.weight = Parameter(torch.zeros(self.num_features))
-		self.bias = Parameter(torch.zeros(self.num_features))
+        # Parameters
+        self.weight = Parameter(torch.zeros(self.num_features))
+        self.bias = Parameter(torch.zeros(self.num_features))
 
-		# Store the average mean and variance
-		self.register_buffer('running_mean', torch.zeros(num_features))
-		self.register_buffer('running_var', torch.ones(num_features))
-		
-		# Initialize your parameter
-		init.ones_(self.weight)
-		init.zeros_(self.bias)
+        # Store the average mean and variance
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
 
-	def forward(self, input):
-		# input: [batch_size, num_feature_map * height * width]
-		if self.train:
-			mean=torch.mean(input=input,dim=0)
-			var=torch.var(input=input, dim=0)
+        # Initialize your parameter
+        init.ones_(self.weight)
+        init.zeros_(self.bias)
 
-			self.running_mean=(1-self.momentum)*self.running_mean+self.momentum*mean
-			self.running_var=(1-self.momentum)*self.running_var+self.momentum*var
+    def forward(self, input):
+        # input: [batch_size, num_feature_map * height * width]
+        if self.train:
+            mean = torch.mean(input=input, dim=0)
+            var = torch.var(input=input, dim=0)
 
-			std_deviation=torch.sqrt(var+1e-10)
-			return ((input-mean)/std_deviation)*self.weight+self.bias
-		else:
-			std_deviation=torch.sqrt(self.running_var+1e-10)
-			return ((input-self.running_mean)/std_deviation)*self.weight+self.bias
-	
-	# TODO END
+            self.running_mean = (1-self.momentum) * \
+                self.running_mean+self.momentum*mean
+            self.running_var = (1-self.momentum) * \
+                self.running_var+self.momentum*var
+
+            std_deviation = torch.sqrt(var+1e-10)
+            return ((input-mean)/std_deviation)*self.weight+self.bias
+        else:
+            std_deviation = torch.sqrt(self.running_var+1e-10)
+            return ((input-self.running_mean)/std_deviation)*self.weight+self.bias
+
+    # TODO END
+
 
 class Dropout(nn.Module):
-	# TODO START
-	def __init__(self, p=0.5):
-		super(Dropout, self).__init__()
-		self.p = p
+    # TODO START
+    def __init__(self, p=0.5):
+        super(Dropout, self).__init__()
+        self.p = p
 
-	def forward(self, input):
-		# input: [batch_size, num_feature_map * height * width]
-		return input
-	# TODO END
+    def forward(self, input):
+        # input: [batch_size, num_feature_map * height * width]
+        return input
+    # TODO END
+
 
 class Model(nn.Module):
-	def __init__(self, drop_rate=0.5):
-		super(Model, self).__init__()
-		# TODO START
-		# Define your layers here
-		self.model=nn.Sequential(
-			('fc1',nn.Linear(3*32*32, 4096)),
-			('bn',BatchNorm1d(4096)),
-			('relu',nn.ReLU()),
-			('fc2',nn.Linear(4096,10))
-		)
-		# TODO END
-		self.loss = nn.CrossEntropyLoss()
+    def __init__(self, drop_rate=0.5):
+        super(Model, self).__init__()
+        # TODO START
+        # Define your layers here
+        self.model = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(3*32*32, 4096)),
+            ('bn', BatchNorm1d(4096)),
+            ('relu', nn.ReLU()),
+            ('fc2', nn.Linear(4096, 10))
+        ]))
+        # TODO END
+        self.loss = nn.CrossEntropyLoss()
 
-	def forward(self, x, y=None):
-		# TODO START
-		# the 10-class prediction output is named as "logits"
-		logits = self.model(x)
-		# TODO END
+    def forward(self, x, y=None):
+        # TODO START
+        # the 10-class prediction output is named as "logits"
+        logits = self.model(x)
+        # TODO END
 
-		pred = torch.argmax(logits, 1)  # Calculate the prediction result
-		if y is None:
-			return pred
-		loss = self.loss(logits, y)
-		correct_pred = (pred.int() == y.int())
-		acc = torch.mean(correct_pred.float())  # Calculate the accuracy in this mini-batch
+        pred = torch.argmax(logits, 1)  # Calculate the prediction result
+        if y is None:
+            return pred
+        loss = self.loss(logits, y)
+        correct_pred = (pred.int() == y.int())
+        # Calculate the accuracy in this mini-batch
+        acc = torch.mean(correct_pred.float())
 
-		return loss, acc
+        return loss, acc
